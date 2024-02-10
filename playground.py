7c32dd4ad0632863d99fbe8a36da1b308050e29b
Shai_Base <shaibase1328@gmail.com>
2024-02-11 00:10:31 +0800

    added a function to keep the text that will be used from the spreadsheet for the template at center. since there are different lengths to our name and info. using x and y position will ruin the layout

--- playground.py
+++ playground.py
@@ -12,7 +12,8 @@ spreadsheet_key = '1YVrx4KBY8XGpz-BpVJzBTKByn494QhbDDfwjeLa6Sik'
 worksheet_name = 'ID Data'
 sheet = client.open_by_key(spreadsheet_key).worksheet(worksheet_name)
 
-font = ImageFont.truetype("arial.ttf", 30)
+font_size = 30
+font = ImageFont.truetype("arial.ttf", font_size)
 
 # collect the data from Google Sheets
 data = sheet.get_all_records()
@@ -26,18 +27,26 @@ for idx, entry in enumerate(data, 1):
     draw = ImageDraw.Draw(id_template)
 
     # Fields are the headers from gsheets
-    name = entry['Name']
+    name = entry['Name ']
     lrn = entry['LRN']
     student_number = entry['Student Number']
     college_program = entry['College Program']
     contact_number = entry['Contact Number']
     emergency_contact_number = entry['Emergency Contact Number']
 
-    def center(draw, text, y_position):
+    # Function to calculate the center position based on text width
+    def center_text(draw, text, y_position):
         text_bbox = draw.textbbox((0, 0), f"{text}", font=font)
         text_width = text_bbox[2] - text_bbox[0]
         x_position = (id_template.width - text_width) // 2
         draw.text((x_position, y_position), f"{text}", font=font, fill='black')
+
+    center_text(draw, name, 535)
+    center_text(draw, f"LRN: {lrn}", 710)
+    center_text(draw, student_number, 575)
+    center_text(draw, college_program, 750)
+    center_text(draw, contact_number, 620)
+    center_text(draw, f"Emergency: {emergency_contact_number}", 810)
 
     # Save the generated ID card
     output_path = f"{name}_ID_Card.jpg"
