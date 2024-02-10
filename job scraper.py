d5657b9df7aa03f7de311cd9b980ce7da82a36d5
Shai_Base <shaibase1328@gmail.com>
2024-02-10 09:04:31 +0800

    made a google sheet API in json file and added google sheets key

--- job scraper.py
+++ job scraper.py
@@ -1,1 +1,13 @@
-print('hello world')
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from PIL import Image, ImageDraw, ImageFont
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('stellar-cipher-413820-564243d36b0b.json', scope)
+client = gspread.authorize(creds)
+
+spreadsheet_key = '1YVrx4KBY8XGpz-BpVJzBTKByn494QhbDDfwjeLa6Sik'
+worksheet_name = 'ID Data'
+sheet = client.open_by_key(spreadsheet_key).worksheet(worksheet_name)
+
+
